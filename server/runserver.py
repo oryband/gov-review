98dbfeccf63012a79650610a806c61cfd04d1df4
Ory Band <oryband@gmail.com>
2012-09-04 23:27:39 +0300

    Started working on basic template

---
+++ server/runserver.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# encoding: utf8
+
+from flask import Flask, render_template, redirect, url_for, request
+from wtforms import Form, TextField, TextArea
+
+
+app.host = '0.0.0.0'
+app.debug = True
+
+ROOT = os.path.abspath(os.path.dirname(__file__))
+DB = '%s/db/data' % ROOT
+
+app = Flask(__name__)
+app.config.from_object(__name__)
+
+
+def init_db():
+    """Load reports from db file."""
+    # TODO
+    pass
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+class Report(Form):
+    id = TextField(u'מס\' דו"חות')
+    cp  = TextField(u'פרק')  # Chapter.
+    ent = TextField(u'גופים מבוקרים')  # Critisized entities.
+    url = TextField(u'קישור')
+    desc = TextArea(u'מעקב')  # Description.
+    res = TextArea(u'החלטת הממשלה')  # Government resolution.
+    r_ent = TextField(u'הגוף המגיב')  # Remarking entity.
+    r_desc = TextArea(u'תוכן ההערה')
+
+@app.route('/add', methods=['GET', 'POST'])
+def add(id):
+    if request.method == 'POST' and form.validate():
+        form = Report(request.form)
+        # TODO: get dada from form & update db.
+        return redirect(url_for('index'))
+    else:
+        return render_template('add.html', form=form)
+
+
+
+if __name__ == '__main__':
+    app.run()
