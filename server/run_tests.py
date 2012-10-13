7832c1b631a7cc528bebf81c6b2b38a81df75d7a
Ory Band <oryband@gmail.com>
2012-10-13 22:43:19 +0200

    Moved test to tests/ dir; Added run_tests.py file.

---
+++ server/run_tests.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# encoding: utf8
+
+from run_server import DB_NAME
+from mongoengine import connect
+
+import unittest
+from tests.test_documents import TestDocuments
+
+
+db_name = '%s_test' % DB_NAME
+c = connect(db_name)
+c.drop_database(db_name)  # flush db.
+unittest.main()
+c.close()
